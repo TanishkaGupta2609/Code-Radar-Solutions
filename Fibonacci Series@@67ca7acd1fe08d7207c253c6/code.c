739d32f11f906cefbe49f824981ae5c8b8a94b04
Tanishka Gupta <tanishkagupta152621@gmail.com>
2025-03-10 12:13:37 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -9,11 +9,12 @@ int fibonacciSeries(int n)
     int fib_n1=fibonacciSeries(n-1);
     int fib_n2=fibonacciSeries(n-2);
     int fib_n=fib_n1+fib_n2;
-    return fib_n;
+    
+    
     for(int i=0;i<n;i++){
         printf("%d",fibonacciSeries(i));
     }
-
+    return fib_n;
 
 }
    
