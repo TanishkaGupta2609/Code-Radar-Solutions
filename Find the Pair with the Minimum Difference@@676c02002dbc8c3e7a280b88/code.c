e16e3cbd60ac4f464ecbd02aa07a52f1f4077292
Tanishka Gupta <tanishkagupta152621@gmail.com>
2025-04-04 17:15:08 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -4,7 +4,7 @@ void MinDiffArray(int arr[],int n){
     int diff_num=arr[1]-arr[0];
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
-            arr[i]-arrr[j]=abs(num);
+            arr[i]-arr[j]=abs(num);
             if(num<diff_num){
                 diff_num=num;
             }
