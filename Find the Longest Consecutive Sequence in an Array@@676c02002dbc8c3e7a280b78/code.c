5a1fdeb9409b0c5ec1c8df30da3933b840b9e3b8
Tanishka Gupta <tanishkagupta152621@gmail.com>
2025-04-07 18:39:27 +0530

    Solution to Find the Longest Consecutive Sequence in an Array

--- Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
+++ Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
@@ -1,28 +1,61 @@
+// #include <stdio.h>
+// void consecutiveArray(int arr[],int n){
+//     int count=1,max_count=1;
+//     for(int i=0;i<n-1;i++){
+//         if(arr[i+1]==arr[i]+1 || arr[i+1]==arr[i]-1){
+//             count++;
+//         }
+//         else if(arr[i]==arr[i+1]){
+//             continue;
+//         }
+//         else{
+//             count=1;
+//         }
+//         if(count>max_count){
+//                 max_count=count;
+//             }
+//     }
+//     printf("%d",max_count);
+// }
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     consecutiveArray(arr,n);
+// }
 #include <stdio.h>
-void consecutiveArray(int arr[],int n){
-    int count=1,max_count=1;
-    for(int i=0;i<n-1;i++){
-        if(arr[i+1]==arr[i]+1 || arr[i+1]==arr[i]-1){
+
+void consecutiveArray(int arr[], int n) {
+    int count = 1, max_count = 1;
+    
+    for (int i = 1; i < n; i++) {
+        if (arr[i] == arr[i - 1] + 1 || arr[i] == arr[i - 1] - 1) {
             count++;
-        }
-        else if(arr[i]==arr[i+1]){
+        } else if (arr[i] == arr[i - 1]) {
             continue;
+        } else {
+            count = 1;
         }
-        else{
-            count=1;
+
+        if (count > max_count) {
+            max_count = count;
         }
-        if(count>max_count){
-                max_count=count;
-            }
     }
-    printf("%d",max_count);
+
+    printf("%d", max_count);
 }
-int main(){
+
+int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    consecutiveArray(arr,n);
+
+    consecutiveArray(arr, n);
 }
