39fca70bcefb8b5035b2d24d109c82154c91f67b
Tanishka Gupta <tanishkagupta152621@gmail.com>
2025-04-08 11:12:46 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,10 +3,13 @@
 int main() {
     char operator;
     float num1, num2, result;
+
+    // Read operator with leading space to ignore leftover newline
     scanf(" %c", &operator);
+    // Read two float numbers
     scanf("%f %f", &num1, &num2);
 
-    switch(operator) {
+    switch (operator) {
         case '+':
             result = num1 + num2;
             printf("%.2f", result);
@@ -20,12 +23,12 @@ int main() {
             printf("%.2f", result);
             break;
         case '/':
-            if(num2 != 0){
+            if (num2 != 0) {
                 result = num1 / num2;
                 printf("%.2f", result);
-            }
-            else
+            } else {
                 printf("Error: Division by zero");
+            }
             break;
         default:
             printf("Invalid operator");
