7394f451b2b73f844697d6919fef46a4eccee4b9
Tanishka Gupta <tanishkagupta152621@gmail.com>
2025-04-08 11:14:30 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,28 +4,27 @@ int main() {
     char operator;
     float num1, num2;
 
-    // Fix: Space before %c ignores whitespace like newline
-    scanf(" %c", &operator);         
+    // Space before %c skips whitespace (like newline)
+    scanf(" %c", &operator);
     scanf("%f %f", &num1, &num2);
 
-    switch (operator) {
-        case '+':
-            printf("%.2f", num1 + num2);
-            break;
-        case '-':
-            printf("%.2f", num1 - num2);
-            break;
-        case '*':
-            printf("%.2f", num1 * num2);
-            break;
-        case '/':
-            if (num2 != 0)
-                printf("%.2f", num1 / num2);
-            else
-                printf("Division by zero error");
-            break;
-        default:
-            printf("Invalid operator");
+    if (operator == '+') {
+        printf("%.2f", num1 + num2);
+    }
+    else if (operator == '-') {
+        printf("%.2f", num1 - num2);
+    }
+    else if (operator == '*') {
+        printf("%.2f", num1 * num2);
+    }
+    else if (operator == '/') {
+        if (num2 != 0)
+            printf("%.2f", num1 / num2);
+        else
+            printf("Division by zero error");
+    }
+    else {
+        printf("Invalid operator");
     }
 
     return 0;
