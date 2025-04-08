528261bf2ac71845113698cab3572ef8063c2d2b
Tanishka Gupta <tanishkagupta152621@gmail.com>
2025-04-08 17:11:23 +0530

    Solution to Toggle Case in a String

--- Toggle Case in a String@@676c039d80e1714600f67650/code.c
+++ Toggle Case in a String@@676c039d80e1714600f67650/code.c
@@ -4,11 +4,11 @@
 void ToggleCase(char name[]){
     int n=strlen(name);
     for(int i=0;i<n;i++){
-        if(name[i]==name.lower){
-            name[i]=name.upper;
+        if(name[i]==tolower(name[i])){
+            name[i]=toupper(name[i]);
         }
-        else if(name[i]=name.upper){
-            name[i]=name.lower;
+        else if(name[i]=toupper(name[i])){
+            name[i]=tolower(name[i]);
         }
 
     }
