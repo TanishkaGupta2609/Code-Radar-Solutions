5ed984d41f8dfe9dd947506b2f68bd1ec1cc5875
Tanishka Gupta <tanishkagupta152621@gmail.com>
2025-04-08 11:16:05 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -21,7 +21,7 @@ int main() {
         if (num2 != 0)
             printf("%d", num1 / num2);
         else
-            printf("Division by zero error");
+            printf("error");
     }
     else {
         printf("Invalid operator");
