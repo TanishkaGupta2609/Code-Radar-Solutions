73e6fcc6ffe082dfccecaac5477aa5a3e0f7c5a4
Tanishka Gupta <tanishkagupta152621@gmail.com>
2025-04-08 11:08:19 +0530

    Solution to Check if a String is a Pangram

---
+++ Check if a String is a Pangram@@67877c59ddaeab38c8ad25b2/code.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sting.h>
+void PanagramString(char arr[]){
+    int arr=arr_int[]
+    int flag=0;
+    for(int i=0;i<n;i++){
+        if(arr_int[i]>=65 && arr_int[i]<90 ||arr_int[i]>=97 && arr_int[i]<122){
+            flag=1;
+        }
+    }
+    if(flag){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+}
+int main(){
+    char arr[500];
+    fgets(arr,500,stdin);
+    PanagramString(arr);
+    return 0;
+}
