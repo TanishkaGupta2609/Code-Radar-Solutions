d4187a55820e87ab230af6bd529560f61ebe7b2e
Tanishka Gupta <tanishkagupta152621@gmail.com>
2025-04-11 00:43:18 +0530

    Solution to Streaming Service Subscription Management

---
+++ Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Subscription {
+    char username[100];
+    char subscription_type[20];
+    float subscription_cost;
+};
+
+int main() {
+    int n;
+    printf("Enter the number of users: ");
+    scanf("%d", &n);
+
+    struct Subscription subscriptions[n];
+    float total_basic = 0, total_standard = 0, total_premium = 0;
+    int count_basic = 0, count_standard = 0, count_premium = 0;
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter details for user %d:\n", i + 1);
+        printf("Username: ");
+        scanf("%s", subscriptions[i].username);
+        printf("Subscription type (Basic/Standard/Premium): ");
+        scanf("%s", subscriptions[i].subscription_type);
+        printf("Subscription cost: ");
+        scanf("%f", &subscriptions[i].subscription_cost);
+
+        // Calculate revenue and count users based on subscription type
+        if (strcmp(subscriptions[i].subscription_type, "Basic") == 0) {
+            total_basic += subscriptions[i].subscription_cost;
+            count_basic++;
+        } else if (strcmp(subscriptions[i].subscription_type, "Standard") == 0) {
+            total_standard += subscriptions[i].subscription_cost;
+            count_standard++;
+        } else if (strcmp(subscriptions[i].subscription_type, "Premium") == 0) {
+            total_premium += subscriptions[i].subscription_cost;
+            count_premium++;
+        }
+    }
+
+    // Display results
+    printf("\nTotal Revenue:\n");
+    printf("Basic: $%.2f from %d users\n", total_basic, count_basic);
+    printf("Standard: $%.2f from %d users\n", total_standard, count_standard);
+    printf("Premium: $%.2f from %d users\n", total_premium, count_premium);
+
+    return 0;
+}
